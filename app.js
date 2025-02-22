fc61e2b491da66512dd932208a4ea192f9895710
Rysioner <amador.20js@gmail.com>
2025-02-21 22:53:00 -0500

    Implementación función para sortear amigo

--- app.js
+++ app.js
@@ -12,16 +12,30 @@ function agregarAmigo() {
     recorrer();
 }
 
-function recorrer(){
+function limpiarLista(){
     let lista = document.getElementById('listaAmigos');
-    //Lo usamos para limpiar lista y así no se repita el nombre
     lista.innerHTML = "";
+}
 
+function recorrer(){
+    limpiarLista();
     for (let i=0; i<amigos.length; i++){
         //Creamos el elemento li y le añadimos el nombre de cada amigo
         let li = document.createElement('li');
         li.textContent = amigos[i];
         //Añadimos cada elemento creado a la lista de amigos
         document.querySelector('#listaAmigos').appendChild(li);
+    }
+    return;
+}
+
+function sortearAmigo(){
+    if (amigos.length == 0) {
+        alert('Lista vacía, por favor añada nombres.');
+    } else {
+        let indiceAleatorio =  Math.floor(Math.random()*amigos.length-1)+1;
+        limpiarLista();
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = `El amigo secreto sorteado es: ${amigos[indiceAleatorio]}`;
     }
 }
